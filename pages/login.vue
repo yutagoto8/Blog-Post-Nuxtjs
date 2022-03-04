c6de152307a45b778f4468f75687b43fc24bbe54
ygoto <y.goto@cybermissions.co.jp>
2022-03-04 17:58:48 +0900

    loginフォームの作成

--- pages/login.vue
+++ pages/login.vue
@@ -1,17 +1,73 @@
 <template>
   <v-container>
     <h1>Login</h1>
-    <v-row>
-      <v-col cols="12">
-
-      </v-col>
-    </v-row>
+    <v-form ref="login_form">
+      <v-text-field
+        v-model="email"
+        type="email"
+        :rules="[required]"
+        label="Email"
+        prepend-icon="mdi-email"
+      />
+      <v-text-field
+        v-model="password"
+        :type="showPassword ? 'text' : 'password'"
+        :rules="[required]"
+        label="Password"
+        prepend-icon="mdi-lock"
+        :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+        @click:append="showPassword = !showPassword"
+      />
+      <v-btn
+        type="submit"
+        @click.prevent="login"
+      >
+        Login
+      </v-btn>
+      <div>
+        {{ message }}
+      </div>
+      <br>
+      <div>
+        <nuxt-link to="/signup">
+          Create Account
+        </nuxt-link>
+      </div>
+    </v-form>
   </v-container>
 </template>
 
 <script>
+import { signInWithEmailAndPassword } from '@firebase/auth'
+import { auth } from '../plugins/firebase'
+
 export default {
-  name: 'loginPage'
+  name: 'LoginPage',
+  data () {
+    return {
+      email: '',
+      name: '',
+      password: '',
+      showPassword: false,
+      required: value => !!value || '必須入力です'
+    }
+  },
+  methods: {
+    login () {
+      if (this.$refs.login_form.validate()) {
+        signInWithEmailAndPassword(auth, this.email, this.password)
+          .then((userCredential) => {
+            this.message = 'Wellcome to BlogPost!!'
+            this.$router.push('/')
+          })
+          .catch((err) => {
+            this.message = err.message
+          })
+      } else {
+        this.message = '入力エラーです！'
+      }
+    }
+  }
 }
 </script>
 
